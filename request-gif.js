9d7d17f02a4625e3f46a9761c78177a6bc7a6c97
Theresa OHare <theresa.ohare@outlook.com>
2019-09-16 13:52:07 -0600

    completed page functionality, added form validation, added basic styling

--- request-gif.js
+++ request-gif.js
@@ -7,38 +7,37 @@ var ajaxson5 = new Vue({
             errorMessage: null,
             loading: false,
             imgSrc: null,
+            validation: ""
         };
     },
-    methods: {
+    methods: { 
         fetchGif: function() {
-            // get the user's input text from the DOM
-            var searchQuery = ""; // TODO should be e.g. "dance"
-
-            // configure a few parameters to attach to our request
-            var api_key = "dc6zaTOxFJmzC";
-            var tag = ""; // TODO should be e.g. "jackson 5 dance"
-
-            // TODO what do we want this URL to be?
-            fetch(`http://example.com?api_key=${api_key}&tag=${tag}`)
-                .then(response => response.ok ? response.json() : Promise.reject(response))
-                .then(results => {
-                    // if the response comes back successfully, the code in here will execute.
-
-                    console.log("we received a response!");
-                    console.log(results);
-                    // TODO
-                    // 1. set the imgSrc value in our data to the GIF's image_url inside results
-                    // 2. clear the error message and loading state (since our request just succeede)
-                })
-                .catch(err => {
-                    // if something went wrong, the code in here will execute instead of the success function
-
-                    this.loading = false;
-                    this.errorMessage = 'Sorry, could not load GIF. Try again!';
+            if (this.validation === "5"){
+                this.loading = true;
+                // get the user's input text from the DOM
+                var searchQuery = this.tagValue; 
+                // configure a few parameters to attach to our request
+                var api_key = "dc6zaTOxFJmzC";
+                var tag = "jackson 5 " + searchQuery; 
+                fetch(`https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=${tag}`)
+                    .then(response => response.ok ? response.json() : Promise.reject(response))
+                    .then(results => {
+                        // if the response comes back successfully, the code in here will execute.
+                        console.log("we received a response!");
+                        this.imgSrc = results.data.image_url;
+                        this.errorMessage = "";
+                        this.loading = false;
+                    })
+                    .catch(err => {
+                        // if something went wrong, the code in here will execute instead of the success function
+                        this.loading = false;
+                        this.errorMessage = 'Sorry, could not load GIF. Try again!';
                 });
-            // TODO We've just made a request, so this is a good time to
-            // set "loading = true"
-
+            } else {
+                this.errorMessage = "No GIFS for you, Robot!";
+                this.loading = false;
+                this.imgSrc = ""
+            }
         },
     },
 });
